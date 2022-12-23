977fb1f4d14957b1c17a5127a162a8d43712082f
Niko <nxenos@me.com>
2022-12-23 12:25:36 +0200

    lesson 53

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,6 +3,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
+const INITIAL_STRING ='Hi there!';
 
 let accounts;
 let inbox;
@@ -13,7 +14,10 @@ beforeEach(async ()=> {
     
     // Use one of those accounts to deploy contract 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode,arguments: ['Hi There!']})
+    .deploy({ 
+        data: bytecode,
+        arguments: [INITIAL_STRING]
+    })
     .send({from: accounts[0],gas:'1000000'})
 
 });
@@ -24,5 +28,11 @@ describe('Inbox', ()=> {
     });
     it('has a default message',async () => {
         const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING)
+    });
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({from: accounts[0]});
+        const messsage = await inbox.methods.message().call();
+        assert.equal(messsage,'bye');
     });
 });
